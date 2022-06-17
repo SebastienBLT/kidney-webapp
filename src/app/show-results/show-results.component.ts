4cf22b33eab57756f85e2eb053bfb3db52bcdf63
MartinGbz <martin.grabarz@outlook.fr>
2022-06-17 18:46:23 +0200

    code cleaning

--- src/app/show-results/show-results.component.ts
+++ src/app/show-results/show-results.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// @ts-ignore
 import results from '../../../../oop-opti-kidney-swap/results.json';
 
 @Component({
@@ -18,86 +17,63 @@ export class ShowResultsComponent implements OnInit {
     console.log(this.jsonResults)
   }
 
-  // @ts-ignore
-  changeInstance(e) {
+  changeInstance(e: any) {
     var idItemSelected = e.target.value;
     this.selectedInstance = this.jsonResults.results[idItemSelected];
 
     console.log(this.selectedInstance);
   }
 
-  checkIsInSolution(itemId: any){
-    for(let chain of this.selectedInstance.solution.chains)
-    {
-      //console.log(chain);
-      //console.log(chain.sequence.find((item: { id: any; }) => item.id == itemId));
-      if(chain.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined){
+  checkIsInSolution(itemId: any) {
+    for(let chain of this.selectedInstance.solution.chains) {
+      if(chain.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined) {
         return false;
       }
     }
-    for(let cycle of this.selectedInstance.solution.cycles)
-    {
-      //console.log(cycle);
-      if(cycle.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined){
+    for(let cycle of this.selectedInstance.solution.cycles) {
+      if(cycle.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined) {
         return false;
       }
     }
     return true;
   }
 
-  getPourcentage(){
+  getPourcentage() {
     let i=this.selectedInstance.instance.altruists.length+this.selectedInstance.instance.pairs.length;
     let itemId: any;
-    for(let elt of this.selectedInstance.instance.altruists){
-      itemId=elt.id;
-      //console.log(i);
-      for(let chain of this.selectedInstance.solution.chains)
-      {
-        //console.log(chain);
-        //console.log(chain.sequence.find((item: { id: any; }) => item.id == itemId));
-        if(chain.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined){
+    for(let elt of this.selectedInstance.instance.altruists) {
+      itemId = elt.id;
+      for(let chain of this.selectedInstance.solution.chains) {
+        if(chain.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined) {
           i--;
         }
       }
-      for(let cycle of this.selectedInstance.solution.cycles)
-      {
-        //console.log(cycle);
-        if(cycle.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined){
+      for(let cycle of this.selectedInstance.solution.cycles) {
+        if(cycle.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined) {
           i--;
         }
       }
-
     }
-    for(let elt of this.selectedInstance.instance.pairs){
+    for(let elt of this.selectedInstance.instance.pairs) {
       itemId=elt.id;
-      for(let chain of this.selectedInstance.solution.chains)
-      {
-        //console.log(chain);
-        //console.log(chain.sequence.find((item: { id: any; }) => item.id == itemId));
-        if(chain.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined){
+      for(let chain of this.selectedInstance.solution.chains) {
+        if(chain.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined) {
           i--;
         }
       }
-      for(let cycle of this.selectedInstance.solution.cycles)
-      {
-        //console.log(cycle);
-        if(cycle.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined){
+      for(let cycle of this.selectedInstance.solution.cycles) {
+        if(cycle.sequence.find((item: { id: any; }) => item.id == itemId)!=undefined) {
           i--;
         }
       }
-
     }
-
     return Math.round(((i/(this.selectedInstance.instance.altruists.length+this.selectedInstance.instance.pairs.length))*100));
   }
 
 
   getTheBestLink(seqList :any){
-    //console.log(sequence);
     let best=0;
-
     for(let seq of seqList){
-      //console.log(seq);
       if(seq.coutVersleSuivant>best){
         best=seq.coutVersleSuivant;
       }
@@ -106,11 +82,9 @@ export class ShowResultsComponent implements OnInit {
   }
 
   getTheWorstLink(seqList :any){
-    //console.log(sequence);
     let best=100000000 ;
 
     for(let seq of seqList){
-      //console.log(seq);
       if(seq.coutVersleSuivant<best){
         best=seq.coutVersleSuivant;
       }
